2be4f43e5c8cc62099199668d73c15bbb8a81392
Zena Hirsch <hirsch.zena@gmail.com>
2026-05-17 14:10:50 -0400

    Display ticket description as plain text
    Removed newline-to-br replacement that was injecting raw HTML into the
    description field. Switched template to double-brace escaping so the
    description is rendered as plain text regardless of its content.

--- src/javascripts/modal.js
+++ src/javascripts/modal.js
@@ -26,7 +26,7 @@ class Modal {
 		let template_data = {
 			id: data.ticket.id,
 			type: data.ticket.type,
-			description: data.ticket.description.replace(/(?:\r\n|\r|\n)/g, '<br />')
+			description: data.ticket.description
 		};
 	
 		this.view.switchTo('ticket_preview', template_data);
